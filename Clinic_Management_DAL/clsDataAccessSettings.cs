a644cabf913347c92210ec4f2919b99e781c22df
Mohamed Benlajdid <benm21158@gmail.com>
2026-03-18 17:02:20 +0000

    Update connection strings for database access

--- Clinic_Management_DAL/clsDataAccessSettings.cs
+++ Clinic_Management_DAL/clsDataAccessSettings.cs
@@ -6,11 +6,11 @@ namespace Clinic_Management_DAL
 {
     static public class clsDataAccessSettings
     {
-        //  static public string ConnectionString2 = "Server=DESKTOP-HCHQ653;Database=HealthcareManagementDB;User Id=sa;Password=123456;";
+        //  static public string ConnectionString2 = "Server=DESKTOP-HCHQ653;Database=HealthcareManagementDB;User Id=username;Password=123456;";
         //static public string ConnectionString1 =
-        //    "Server=DESKTOP-VH7SLCO;Database=Clinic_DB;User Id=sa;Password=123456;TrustServerCertificate=True";
+        //    "Server=DESKTOP;Database=Clinic_DB;User Id=sa;Password=123456;TrustServerCertificate=True";
         static public string ConnectionString =
-        "Server=172.16.186.135,1433;Database=Clinic_DB;User Id=ClinicAppUser;Password=123456;TrustServerCertificate=True;";
+        "Server=IpServer,Port;Database=Clinic_DB;User Id=ClinicAppUser;Password=123456;TrustServerCertificate=True;";
 
     }
 
